15ae504146085e7e63ddbafaf37267152c1af8b9
yaokunhui <1712792360@qq.com>
2022-09-14 20:38:57 +0800

    1

--- jiudsejhsjhg-lodash.js
+++ jiudsejhsjhg-lodash.js
@@ -1,19 +1,18 @@
 var yaokunhui = {
-  chunk:
-  function(array, size = 1) {
-    var temp = new Array(Math.ceil(array.length / size))
-    var a = 0
-    for (var i = 0; i < array.length; i++) {
-        temp[a] ? temp[a] : temp[a] = []
-        temp[a].push(array[i])
-        if (temp[a].length == size) {
-            a++
+    chunk(array, size = 1) {
+        var temp = new Array(Math.ceil(array.length / size))
+        var a = 0
+        for (var i = 0; i < array.length; i++) {
+            temp[a] ? temp[a] : temp[a] = []
+            temp[a].push(array[i])
+            if (temp[a].length == size) {
+                a++
+            }
         }
-    }
-    return temp
-    }
+        return temp
+        }
 
+       
 
-  
 
 }
